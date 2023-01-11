35e43473de55af83bcf11f27fc8ba8b8643c3f08
DongThach <thachdong270293@gmail.com>
2023-01-11 14:14:25 +0700

    add page: purchase plan detail

--- src/modules/dashboard/components/purchase/index.ts
+++ src/modules/dashboard/components/purchase/index.ts
@@ -3,3 +3,4 @@ export * from "./supplier/InfoCreateSupplier";
 export * from "./supplier/InfoProductSupplier";
 export * from "./supplier/RulesSupplier";
 export * from "./purchase-request";
+export * from "./purchase-plan";
--- src/modules/dashboard/pages/purchase/purchase-plan/data.ts
+++ src/modules/dashboard/pages/purchase/purchase-plan/data.ts
@@ -31,3 +31,20 @@ export const purchasePlanColumns: TGridColDef[] = [
   { field: "statusName", headerName: "Trạng thái", minWidth: 120 },
   { field: "supplierName", headerName: "Tên NCC", minWidth: 200, flex: 1 },
 ];
+
+export const dialogColumns: TGridColDef[] = [
+  {
+    field: "STT",
+    headerName: "STT",
+    width: 50
+  },
+  { field: "branchCode", headerName: "MÃ SẢN PHẨM", minWidth: 120 },
+  { field: "mainOrderCode", headerName: "MÔ TẢ", minWidth: 120, flex: 1 },
+  { field: "warehouseSessionCode", headerName: "HÃNG SẢN XUẤT", minWidth: 150 },
+  { field: "nameProduct", headerName: "QUY CÁCH", minWidth: 100 },
+  { field: "count", headerName: "ĐƠN VỊ", minWidth: 100 },
+  { field: "codeSupplier", headerName: "SỐ LƯỢNG", minWidth: 100 },
+  { field: "status", headerName: "ĐƠN GIÁ", minWidth: 100, renderCell: ({row}) => _format.getVND(row?.price) },
+  { field: "nameSupplier", headerName: "THÀNH TIỀN", minWidth: 120 },
+  { field: "note", headerName: "GHI CHÚ", minWidth: 120 },
+];
