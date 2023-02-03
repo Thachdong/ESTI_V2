f29f7a0b1f4f2f965d34205a1c812bfbc0b4ec32
DongThach <thachdong270293@gmail.com>
2023-02-03 16:19:41 +0700

    completly remove next-auth

--- src/types/_app.d.ts
+++ src/types/_app.d.ts
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import { EmotionCache } from "@emotion/cache";
 import { NextPage } from "next";
-import { Session } from "next-auth";
 import { ReactElement } from "react";
 
 type TNextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
