84169a0692f890058a6f6262dcb3b0caf987b929
DongThach <thachdong270293@gmail.com>
2023-02-09 17:31:44 +0700

    add feature: update customer account status

--- src/modules/dashboard/components/account/customers/CustomersReceiveInfoForm.tsx
+++ src/modules/dashboard/components/account/customers/CustomersReceiveInfoForm.tsx
@@ -1,8 +1,19 @@
 import { Box, Collapse, List, ListItemButton, Typography } from "@mui/material";
+import React from "react";
 import { useCallback, useState } from "react";
 import { useFieldArray, useFormContext } from "react-hook-form";
-import { AddButton, DeleteButton } from "~modules-core/components";
+import { useMutation } from "react-query";
+import { customer, TActivateCustomer } from "src/api";
+import {
+  AddButton,
+  AutoCompleteBase,
+  DeleteButton,
+  FormSelect,
+} from "~modules-core/components";
+import { accountStatus } from "~modules-core/constance";
+import { toast } from "~modules-core/toast";
 import { CustomersBill } from "./CustomersBill";
+import { CustomersContactBox } from "./CustomersContactBox";
 import { CustomersCurator } from "./CustomersCurator";
 import { CustomersReceiver } from "./CustomersReceiver";
 
@@ -11,14 +22,19 @@ type TProps = {
   type: string;
 };
 
-export const CustomersReceiveInfoForm: React.FC<TProps> = ({ type, isDisable }) => {
+export const CustomersReceiveInfoForm: React.FC<TProps> = ({
+  type,
+  isDisable,
+}) => {
+  const [status, setStatus] = useState<number>();
+
   const [collapses, setCollapses] = useState<number[]>([0]);
 
   const [curators, setCurators] = useState<any[]>([{}]);
 
-  const { control } = useFormContext();
-  
-  const {remove, append} = useFieldArray({
+  const { control, watch } = useFormContext();
+
+  const { remove, append } = useFieldArray({
     control,
     name: "curatorCreate",
   });
@@ -54,37 +70,14 @@ export const CustomersReceiveInfoForm: React.FC<TProps> = ({ type, isDisable }) 
     <Box>
       <List className="pt-0">
         {curators.map((curator: any, index: number) => (
-          <Box className="!border-grey-2 !rounded-[4px] mb-2">
-            <ListItemButton
-              component="fieldset"
-              onClick={() => handleCollapse(index)}
-              className="border-b-2 border-solid border-[#e9e9e9] mb-3"
-            >
-              <Typography className="font-semibold w-full">{`Thông tin liên hệ ${
-                index + 1
-              }`}</Typography>
-
-              {index > 0 && (
-                <DeleteButton
-                  color="error"
-                  onClick={() => handleRemove(index)}
-                />
-              )}
-            </ListItemButton>
-
-            <Collapse
-              in={collapses.includes(index)}
-              timeout="auto"
-              unmountOnExit
-              className="w-full"
-            >
-              <CustomersCurator isDisable={isDisable} index={index} />
-
-              <CustomersReceiver type={type} isDisable={isDisable} index={index} />
-
-              <CustomersBill type={type} isDisable={isDisable} index={index} />
-            </Collapse>
-          </Box>
+          <React.Fragment key={index}>
+            <CustomersContactBox
+              isDisable={isDisable}
+              type={type}
+              index={index}
+              handleRemove={handleRemove}
+            />
+          </React.Fragment>
         ))}
       </List>
 
