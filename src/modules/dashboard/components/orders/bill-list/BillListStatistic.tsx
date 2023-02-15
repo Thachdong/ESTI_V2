365c0482212d988a1c4bf794677b4718d847fd2b
DongThach <thachdong270293@gmail.com>
2023-02-15 16:37:35 +0700

    update bill statistic

--- src/modules/dashboard/components/orders/bill-list/BillListStatistic.tsx
+++ src/modules/dashboard/components/orders/bill-list/BillListStatistic.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import { useQuery } from "react-query";
 import { bill } from "src/api";
 import { CardReport } from "~modules-core/components";
+import { _format } from "~modules-core/utility/fomat";
 
 export const BillListStatistic: React.FC = () => {
   const { data } = useQuery(["BillStatisticData"], () =>
@@ -9,19 +10,27 @@ export const BillListStatistic: React.FC = () => {
   );
 
   return (
-    <Box className="mb-4 grid grid-cols-4 gap-4 ">
-      <CardReport title={"TỔNG GIÁ TRỊ HOÁ ĐƠN"} BgImage={"Orange"} value={0} />
+    <Box className="mb-4 grid grid-cols-2 xl:grid-cols-4 gap-4 ">
+      <CardReport
+        title={"TỔNG GIÁ TRỊ HOÁ ĐƠN"}
+        BgImage={"Orange"}
+        value={_format.getVND(data?.tongGiaTri)}
+      />
       <CardReport
         title={"TỔNG GIÁ TRỊ ĐÃ THANH TOÁN"}
         BgImage={"Green"}
-        value={0}
+        value={_format.getVND(data?.daThanhToan)}
       />
       <CardReport
         title={"TỔNG GIÁ TRỊ CÒN PHẢI THU"}
         BgImage={"Black"}
-        value={0}
+        value={_format.getVND(data?.conLai)}
       />
-      <CardReport title={"TỔNG GIÁ TRỊ QUÁ HẠN"} BgImage={"Red"} value={0} />
+      <CardReport
+        title={"TỔNG GIÁ TRỊ QUÁ HẠN"}
+        BgImage={"Red"}
+        value={_format.getVND(data?.quaHan)}
+      />
     </Box>
   );
 };
